6c1dfa5bd7326c29c8c97dc8b74f050445dc313a
HassanAMian <hassanmian@hassanmian.net>
2014-11-12 20:26:22 -0500

    asset changes

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,9 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+config.serve_static_assets = true
+config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+config.assets.compile = true
 
 module Todo
   class Application < Rails::Application
